1de980bc82e6f83896bd578644eaf3f4248633d7
TANIKA <2023ucp1847@mnit.ac.in>
2024-10-07 15:19:22 +0530

    ADDED CPP FILE

---
+++ vec.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dotProduct(int vecA[], int vecB[])
+{
+    int product = 0;
+    for (int i = 0; i < 3; i++)
+        product = product + vecA[i]*vecB[i];
+        
+    return product;
+}
+
+int main()
+{
+
+    int vecA[] = {4, -5, 4};
+    int vecB[] = {2, 6, 5};
+    
+    cout << "DOT PRODUCT: ";
+    cout << dotProduct(vecA, vecB) << endl;
+
+    return 0;
+}
+
